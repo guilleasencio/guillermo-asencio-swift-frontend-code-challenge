90de623c1251dcd6c24eb4893be5a575ef23545a
Guillermo Asencio <guillermo.asencio.sanchez@gmail.com>
2024-04-26 21:16:42 +0200

    Create Data package

---
+++ Data/Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Data",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Data",
+            targets: ["Data"]),
+    ],
+    dependencies: [
+        .package(path: "../Domain"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.9.1"),
+        .package(url: "https://github.com/realm/SwiftLint", from: "0.54.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Data",
+            dependencies: [
+                "Domain",
+                .product(name: "Alamofire", package: "alamofire")
+            ],
+            plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]),
+        .testTarget(
+            name: "DataTests",
+            dependencies: ["Data"],
+            plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]),
+    ]
+)
